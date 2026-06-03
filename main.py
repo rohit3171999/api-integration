639b8275f0bb375164dca6fdf5b2fe46274a5f2a
Rohit Sharma <rohit3171999@gmail.com>
2026-06-03 13:41:25 +0530

    feat: add items endpoint to return a list of items

--- main.py
+++ main.py
@@ -11,6 +11,16 @@ students = [
     {"id": 3, "name": "Priya"}
 ]
 
+items = [
+    {"id": 1, "name": "x"},
+    {"id": 2, "name": "y"},
+    {"id": 3, "name": "z"}
+]
+
 @app.get("/students")
 def get_students():
     return students 
+
+@app.get("/items")
+def get_items():
+    return items 
